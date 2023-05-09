f2193621631be798709100b2f7ae9fe360d6b272
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2023-05-09 17:01:10 +0000

    2

--- src/main/java/com/epam/rd/autotasks/matrix/MatrixUtil.java
+++ src/main/java/com/epam/rd/autotasks/matrix/MatrixUtil.java
@@ -5,8 +5,31 @@ import java.util.Arrays;
 public class MatrixUtil {
 
 	public static void transformMatrix(int[][] matrix) {
-		// TODO: Implement this method.
+		     if (matrixHasNull(matrix) || matrix[0].length != matrix[1].length || matrix.length % 2 != 0 ) {
+            System.out.println(Arrays.deepToString(matrix));
+        } else {
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    if (i < j) {
+                        matrix[i][j] = 1;
+                    } else if (i > j) {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+            for (int[] ints : matrix) {
+                System.out.println(Arrays.toString(ints));
+            }
+        }
 	}
+	private static boolean matrixHasNull(int[][] matrix) {
+        for (int[] ints : matrix) {
+            if (ints == null || ints.length == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 	public static void main(String[] args) {
 		{
